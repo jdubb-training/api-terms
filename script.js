5983bea37bc9a108bd15ba7112b919956c3609c5
jdubb-training <122222542+jdubb-training@users.noreply.github.com>
2023-12-22 14:55:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((csv) => {
             // Parse the CSV data
             data = parseCSV(csv);
+
+            // Sort the data by "Category," "Sub-Category," and "Term" columns
+            sortByDefault();
+
             initializeTable();
         })
         .catch((error) => {
@@ -15,9 +19,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const tableBody = document.getElementById("table-body");
     const paginationControls = document.getElementById("pagination");
+    const searchInput = document.getElementById("search-input");
+    const searchButton = document.getElementById("search-button");
 
     let currentPage = 1;
-    const rowsPerPage = 20;
+    const rowsPerPage = 10;
 
     function updateTable() {
         const start = (currentPage - 1) * rowsPerPage;
@@ -46,10 +52,57 @@ document.addEventListener("DOMContentLoaded", function () {
         updateTable();
     }
 
+    function sortByDefault() {
+        // Sort by "Category," "Sub-Category," and "Term" columns
+        data.sort((a, b) => {
+            const categoryIndex = data[0].indexOf("Category");
+            const subCategoryIndex = data[0].indexOf("Sub-Category");
+            const termIndex = data[0].indexOf("Term");
+
+            const categoryComparison = a[categoryIndex].localeCompare(b[categoryIndex]);
+            if (categoryComparison !== 0) return categoryComparison;
+
+            const subCategoryComparison = a[subCategoryIndex].localeCompare(b[subCategoryIndex]);
+            if (subCategoryComparison !== 0) return subCategoryComparison;
+
+            return a[termIndex].localeCompare(b[termIndex]);
+        });
+    }
+
     function initializeTable() {
         updateTable();
         paginationControls.style.display = "block";
     }
+
+    function searchByTerm() {
+        const searchTerm = searchInput.value.trim().toLowerCase();
+
+        if (searchTerm === "") {
+            // If the search input is empty, reset the table
+            sortByDefault();
+        } else {
+            // Filter the data based on the search term in the "Term" column
+            data = data.filter((row, index) => {
+                if (index === 0) return true; // Include the header row
+                const termIndex = data[0].indexOf("Term");
+                return row[termIndex].toLowerCase().includes(searchTerm);
+            });
+        }
+
+        // Reset the current page to 1 and update the table
+        currentPage = 1;
+        updateTable();
+    }
+
+    // Event listener for the search button
+    searchButton.addEventListener("click", searchByTerm);
+
+    // Event listener for the Enter key in the search input field
+    searchInput.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            searchByTerm();
+        }
+    });
 
     document.getElementById("prev-page").addEventListener("click", () => {
         if (currentPage > 1) {
